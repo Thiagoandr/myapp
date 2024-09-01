3e21a2145289849bdf9269bab5793fd17eaa908f
andradet <andradet@pge.rj.gov.br>
2024-09-01 18:46:35 -0300

    feat: criação dde teste icon para produção

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,8 @@
 import { Text, View } from 'react-native';
 
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
+
 export default function App() {
     return (
         <View style={{
@@ -7,19 +10,30 @@ export default function App() {
             backgroundColor: '#131016',
             padding: 24
         }}>
-            <Text style={{
-                color: '#fff',
-                fontSize: 24,
-                fontWeight: 'bold',
+            <View style={{
+                flexDirection: 'row', 
+                alignItems: 'center',  
                 marginTop: 48,
             }}>
-                Nome do evento
-            </Text>
+                <Text style={{
+                    color: '#fff',
+                    fontSize: 24,
+                    fontWeight: 'bold',
+                }}>
+                    Nome do evento 
+                </Text>
+                <MaterialCommunityIcons 
+                    name="calendar-month" 
+                    size={24} 
+                    color="#d3d3d3" 
+                    style={{ marginLeft: 8 }} 
+                />
+            </View>
 
             <Text style={{
                 color: '#6b6b6b',
                 fontSize: 16,
-                marginTop: 5, 
+                marginTop: 4, 
             }}>
                 Domingo, 1/08/2024
             </Text>
